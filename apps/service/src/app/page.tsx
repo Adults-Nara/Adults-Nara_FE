44f1bd57ea5a6e986bf24e12d2f201b51beea91f
tree0-0 <gkdlrne97@naver.com>
2026-02-16 01:24:58 +0900

    [ASN-40] feat: shadcnUI 세팅 및 버튼 컴포넌트 구현

--- apps/service/src/app/page.tsx
+++ apps/service/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Logo } from '@repo/ui';
+import { Button, Logo, SearchIcon } from '@repo/ui';
 
 export default function Home() {
   return (
@@ -13,6 +13,16 @@ export default function Home() {
       <div className="body2">body2</div>
       <div className="body3 text-primary-700">body3</div>
       <div className="body4 text-gray-500">body4</div>
+
+      <Button className="w-fit">
+        <SearchIcon className="" /> 아하
+      </Button>
+      <Button variant={'outline'} size={'lg'}>
+        <SearchIcon /> 아하
+      </Button>
+      <Button variant={'outline'} size={'sm'} className="w-fit border-none">
+        <SearchIcon className="h-3 w-3" /> 아하
+      </Button>
     </>
   );
 }
