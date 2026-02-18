5712a374434e562d60236248c8a8c73e7458ed04
tree0-0 <gkdlrne97@naver.com>
2026-02-18 23:43:56 +0900

    [ASN-46] fix: 검색페이지 Suspense 처리

--- apps/service/src/app/(nav-only)/search/page.tsx
+++ apps/service/src/app/(nav-only)/search/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from 'react';
 import InputHeader from './_components/InputHeader';
 import SearchList from './_components/SearchList';
 
 const page = () => {
   return (
     <div className="flex flex-col">
-      <InputHeader />
-      <SearchList />
+      <Suspense fallback={<div>검색창 불러오는 중...</div>}>
+        <InputHeader />
+        <SearchList />
+      </Suspense>
     </div>
   );
 };
