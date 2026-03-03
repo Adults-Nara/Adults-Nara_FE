c40b003e41287a96da2b3694a0cc73e81d5dd616
tree0-0 <gkdlrne97@naver.com>
2026-03-03 10:31:17 +0900

    [ASN-92] feat: 로그인,회원가입 react-hook-form 추가

--- apps/admin/src/components/auth/LoginForm.tsx
+++ apps/admin/src/components/auth/LoginForm.tsx
@@ -2,6 +2,7 @@
 import { ROUTES } from '@/constant/routes';
 import { Button, Input, Logo } from '@repo/ui';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 interface LoginFormValues {
   email: string;
@@ -9,8 +10,21 @@ interface LoginFormValues {
 }
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>();
+
+  const onSubmit = (data: LoginFormValues) => {
+    console.log(data);
+    // API 요청
+  };
   return (
-    <div className="flex h-162.5 w-125 min-w-125 flex-col items-center justify-center gap-8 rounded-lg border border-gray-500 bg-white px-12.5 shadow-[0_2px_4px_0px_rgba(0,0,0,0.25)]">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex h-162.5 w-125 min-w-125 flex-col items-center justify-center gap-8 rounded-lg border border-gray-500 bg-white px-12.5 shadow-[0_2px_4px_0px_rgba(0,0,0,0.25)]"
+    >
       <div className="flex w-full flex-col items-center gap-5">
         <Logo className="h-23 w-23" />
         <div className="flex flex-col items-center gap-3">
@@ -24,7 +38,19 @@ const LoginForm = () => {
       <div className="flex w-full flex-col gap-5">
         <label>
           이메일
-          <Input className="mt-2" placeholder="admin@example.com" />
+          <Input
+            className="mt-2"
+            placeholder="admin@example.com"
+            {...register('email', {
+              required: '이메일을 입력해주세요.',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: '이메일 형식이 올바르지 않습니다.',
+              },
+            })}
+            error={!!errors.email}
+            errorMessage={errors.email?.message}
+          />
         </label>
         <label>
           비밀번호
@@ -32,6 +58,11 @@ const LoginForm = () => {
             className="mt-2"
             type="password"
             placeholder="비밀번호를 입력하세요"
+            {...register('password', {
+              required: '비밀번호를 입력해주세요.',
+            })}
+            error={!!errors.password}
+            errorMessage={errors.password?.message}
           />
         </label>
       </div>
@@ -45,7 +76,7 @@ const LoginForm = () => {
           </Link>
         </span>
       </div>
-    </div>
+    </form>
   );
 };
 
