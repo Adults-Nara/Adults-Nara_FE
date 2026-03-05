571e7779c146ffbd09ddd0f71b393b6a19add4d3
tree0-0 <gkdlrne97@naver.com>
2026-03-05 13:41:09 +0900

    [ASN-97] feat: 서비스 리프레쉬토큰 프로바이더 구현 및 로컬 https적용

---
+++ apps/service/src/components/auth/AuthProvider.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { API_ENDPOINTS } from '@/constant/endpoints';
+import { httpClient } from '@/services/httpClient';
+import { useAuthStore } from '@/store/useAuthStore';
+import { ApiResponse } from '@/types/api';
+import { useEffect, useState } from 'react';
+
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const [isInitialized, setIsInitialized] = useState(false);
+  const { accessToken } = useAuthStore.getState();
+
+  useEffect(() => {
+    const initAuth = async () => {
+      try {
+        // 이미 토큰이 있으면(페이지 이동 등) 패스
+        if (useAuthStore.getState().accessToken) return;
+
+        const response = await httpClient<ApiResponse<{ accessToken: string }>>(
+          API_ENDPOINTS.AUTH.REFRESH,
+          {
+            method: 'POST',
+            credentials: 'include',
+          },
+        );
+        const accessToken = response.data.accessToken;
+
+        if (accessToken) {
+          setAccessToken(accessToken);
+        } else {
+          console.error('엑세스토큰 없음');
+        }
+      } catch (e) {
+        console.error('인증 초기화 실패');
+      } finally {
+        setIsInitialized(true);
+      }
+    };
+    console.log('레이아웃 엑세스토큰 검증', accessToken);
+    initAuth();
+  }, [setAccessToken]);
+
+  // 초기 로딩 시 깜빡임 방지 (화이트아웃 방지를 위해 스켈레톤이나 null 리턴)
+  if (!isInitialized) return null;
+
+  return <>{children}</>;
+}
--- apps/service/src/components/auth/index.ts
+++ apps/service/src/components/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as LoginButton } from './LoginButton';
 export { default as KakaoCallback } from './KakaoCallback';
+export { default as AuthProvider } from './AuthProvider';
