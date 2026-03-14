55d4ceda35a639d0f834f4f2d41896717688da6a
0dimen <first11@konkuk.ac.kr>
2026-03-15 00:10:53 +0900

    fix: 광고 영상 0초부터 시작 중복 조건 제거

--- apps/service/src/app/(blank)/long/_components/VideoPlayer.tsx
+++ apps/service/src/app/(blank)/long/_components/VideoPlayer.tsx
@@ -200,8 +200,8 @@ export function VideoPlayer({
         playbackRate={playbackRate}
         controls={false}
         onReady={() => {
-          // 광고 모드가 아닐 때만 초기 재생 위치 설정
-          if (!isAdMode && playerRef.current && progress > 0) {
+          // 광고 모드가 아니면, progress가 0보다 클 때에만 시점을 이동
+          if (playerRef.current && progress > 0) {
             playerRef.current.currentTime = progress;
           }
         }}
